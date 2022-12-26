d7c4dec7515ffdb8e8062538e00781c3ca327cdc
isots-code <23300310+isots-code@users.noreply.github.com>
2022-12-26 01:19:09 +0000

    Switch interpolation options

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "ffmpegDecode.h"
 
 int main(int argc, char** argv) {
+	(void)argc;
 
 	try {
 		auto decoder = ffmpegDecode(std::string(argv[1]));
@@ -17,16 +18,33 @@ int main(int argc, char** argv) {
 		AlignedVector<uint8_t> out1(dim * dim * 2 * 3);
 
 		frameData* dataLookup;
-		//dataLookup = new frameDataCustom(dim, 4, frameData::BITS_8, &centripetalCatMullRomInterpolation);
-		//dataLookup = new lanczosN(dim, 24, frameData::BITS_8);
-		//dataLookup = new lanczosN(dim, 10, frameData::BITS_8);
-		//dataLookup = new lanczosN(dim, 8, frameData::BITS_8);
-		dataLookup = new lanczos4(dim, frameData::BITS_8);
-		//dataLookup = new lanczos3(dim, frameData::BITS_8);
-		//dataLookup = new lanczos2(dim, frameData::BITS_8);
-		//dataLookup = new cubic(dim, frameData::BITS_8);
-		//dataLookup = new linear(dim, frameData::BITS_8);
-		//dataLookup = new nearest(dim, frameData::BITS_8);
+		switch (std::atoi(argv[2])) {
+			case 1:
+				dataLookup = new nearest(dim, frameData::BITS_8);
+				break;
+			case 2:
+				dataLookup = new linear(dim, frameData::BITS_8);
+				break;
+			case 3:
+				dataLookup = new cubic(dim, frameData::BITS_8);
+				break;
+			case 4:
+				dataLookup = new lanczos2(dim, frameData::BITS_8);
+				break;
+			case 5:
+				dataLookup = new lanczos3(dim, frameData::BITS_8);
+				break;
+			case 6:
+			default:
+				dataLookup = new lanczos4(dim, frameData::BITS_8);
+				break;
+			case 7:
+				dataLookup = new lanczosN(dim, std::atoi(argv[3]), frameData::BITS_8);
+				break;
+			case 8:
+				dataLookup = new frameDataCustom(dim, 4, frameData::BITS_8, &centripetalCatMullRomInterpolation);
+				break;
+		}
 		dataLookup->setOBuffers(out0.data(), out1.data());
 
 		decoder.connectFrameData(*dataLookup);
@@ -35,7 +53,7 @@ int main(int argc, char** argv) {
 
 		delete dataLookup;
 
-		
+
 	} catch (std::exception& e) {
 		std::cerr << e.what();
 		return -1;
