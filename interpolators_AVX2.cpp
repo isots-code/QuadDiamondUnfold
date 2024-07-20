d5e0e7d41ecc8cdd543c34be3ebde0936b82e846
isots-code <23300310+isots-code@users.noreply.github.com>
2024-07-20 03:30:50 +0100

    Fix SIMD Centripetal Catmull-ROM interpolation

--- interpolators_AVX2.cpp
+++ interpolators_AVX2.cpp
@@ -3,17 +3,28 @@
 #include "vectormath_hyp.h"
 #include "vectormath_trig.h"
 
+template <typename T>
+Vec8f gather(const T* in, const Vec8i index) {
+	if constexpr (std::same_as<T, float>)
+		return _mm256_i32gather_ps(in, index, sizeof(*in));
+	else {
+		auto mask = Vec8i(UINT_MAX >> 8 * (4 - sizeof(T)));
+		return to_float(mask & _mm256_i32gather_epi32(in, index, sizeof(*in)));
+	}
+}
+
 void centripetalCatMullRomInterpolation_AVX2(const bool op, const int width, const int lenghtJ, const int i, const float* __restrict in, int* __restrict out) {
-	const Vec8f Dj(op ? 1.0f : lenghtJ / (float)width);
+	const Vec8f Dj(op ? (float)width / lenghtJ : lenghtJ / (float)width);
 	Vec8f x = Dj * (i + Vec8f(0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f));
+	x = (x < width + 2) & x;
 	Vec8f x_floor = floor(x);
 	x -= x_floor;
 	Vec8i x_int = truncatei(x_floor);
 
-	Vec8f x_1 = lookup8(x_int - 1 - x_int[0], Vec8f().load(in + x_int[0]));
-	Vec8f x0 = lookup8(x_int + 0 - x_int[0], Vec8f().load(in + x_int[0]));
-	Vec8f x1 = lookup8(x_int + 1 - x_int[0], Vec8f().load(in + x_int[0]));
-	Vec8f x2 = lookup8(x_int + 2 - x_int[0], Vec8f().load(in + x_int[0]));
+	Vec8f x_1 = gather(in, x_int - 1);
+	Vec8f x0 = gather(in, x_int);
+	Vec8f x1 = gather(in, x_int + 1);
+	Vec8f x2 = gather(in, x_int + 2);
 
 	Vec8f res = [](Vec8f x0, Vec8f x1, Vec8f x2, Vec8f x3, Vec8f x) -> Vec8f {
 
